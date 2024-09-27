12018e464e9f070c3cf8e64f5bc1158042dac315
Andy Fillebrown <andy@docEdub.com>
2024-09-26 23:05:02 -0400

    Update CMake scripts for single Cabbage build target usage

--- cmake/fetch_dependencies.cmake
+++ cmake/fetch_dependencies.cmake
@@ -1,149 +1,173 @@
 include_guard()
 
+# The fetch_github_dependency macro fetches a dependency from a GitHub repository or a .tar.gz file.
+#
+# Example usage:
+#[[
+    fetch_github_dependency(
+        choc
+
+        GIT_REPOSITORY https://github.com/Tracktion/choc.git
+
+        # When changing the GIT_TAG, the URL_MD5 must be updated to the new expected value.
+        GIT_TAG 85149958b6d0e51885eefba8816b51798570b54b
+
+        # Comment out this line to fail the MD5 check and print the expected value.
+        URL_MD5 ffdb942dcaa64a716239d37c620e5eb2
+
+        SOURCE_DIR "${FETCHCONTENT_BASE_DIR}/choc-src/include/choc"
+
+        # Uncomment this line to use the Git repository instead of the Github .tar.gz.
+        # This makes development easier but slows the fetch down alot, so don't commit it uncommented.
+        # USE_GIT
+    )
+]]
+macro(fetch_github_dependency name)
+    string(TOLOWER "${name}" name)
+    message(TRACE "Macro: fetch_github_dependency(${name}) ...")
+
+    cmake_parse_arguments(ARG "USE_GIT" "GIT_REPOSITORY;GIT_TAG;URL;URL_MD5;SOURCE_DIR" "" ${ARGN})
+    message(TRACE "Macro: fetch_github_dependency(${name}) - ARG_USE_GIT = ${ARG_USE_GIT}")
+    message(TRACE "Macro: fetch_github_dependency(${name}) - ARG_GIT_REPOSITORY = ${ARG_GIT_REPOSITORY}")
+    message(TRACE "Macro: fetch_github_dependency(${name}) - ARG_GIT_TAG = ${ARG_GIT_TAG}")
+    message(TRACE "Macro: fetch_github_dependency(${name}) - ARG_URL_MD5 = ${ARG_URL_MD5}")
+
+    if(NOT ARG_SOURCE_DIR)
+        set(ARG_SOURCE_DIR "${FETCHCONTENT_BASE_DIR}/${name}-src")
+    endif()
+    message(TRACE "Macro: fetch_github_dependency(${name}) - ARG_SOURCE_DIR = ${ARG_SOURCE_DIR}")
+
+    if(${ARG_USE_GIT})
+        if(DEFINED IS_CABBAGE_CI_BUILD)
+            message(FATAL_ERROR "USE_GIT is not supported for CI builds. Comment it out for dependency \"${name}\" to use the Github .tar.gz instead of git clone.")
+        endif()
+
+        if(NOT ${name}_USE_GIT AND EXISTS "${ARG_SOURCE_DIR}")
+            message(TRACE "Macro: fetch_github_dependency(${name}) - Removing ${ARG_SOURCE_DIR} to fetch the dependency from the Git repository.")
+            execute_process(COMMAND ${CMAKE_COMMAND} -E remove_directory "${ARG_SOURCE_DIR}")
+        endif()
+        set(${name}_USE_GIT true CACHE INTERNAL "" FORCE)
+
+        FetchContent_Declare(
+            ${name}
+            GIT_REPOSITORY "${ARG_GIT_REPOSITORY}"
+            GIT_TAG "${ARG_GIT_TAG}"
+            SOURCE_DIR "${ARG_SOURCE_DIR}"
+        )
+    else()
+        if(NOT ARG_URL)
+            # Convert the git repository url and tag to the .tar.gz url provided by GitHub.
+            string(REGEX REPLACE "^https://(.+)/(.+).git/(.*)$" "https://codeload.\\1/\\2/tar.gz/\\3" ARG_URL "${ARG_GIT_REPOSITORY}/${ARG_GIT_TAG}")
+        endif()
+        message(TRACE "Macro: fetch_github_dependency(${name}) - ARG_URL = ${ARG_URL}")
+
+        # If the URL_MD5 argument is not provided, set it to 1 to fail the check and print the expected value.
+        if(NOT ARG_URL_MD5)
+            set(ARG_URL_MD5 1)
+        endif()
+
+        if(${name}_USE_GIT AND EXISTS "${ARG_SOURCE_DIR}")
+            message(TRACE "Macro: fetch_github_dependency(${name}) - Removing ${ARG_SOURCE_DIR} to fetch the dependency from the Github .tar.gz.")
+            execute_process(COMMAND ${CMAKE_COMMAND} -E remove_directory "${ARG_SOURCE_DIR}")
+        endif()
+        set(${name}_USE_GIT false CACHE INTERNAL "" FORCE)
+
+        FetchContent_Declare(
+            ${name}
+            URL "${ARG_URL}"
+            URL_MD5 "${ARG_URL_MD5}"
+            SOURCE_DIR "${ARG_SOURCE_DIR}"
+            DOWNLOAD_DIR "${FETCHCONTENT_BASE_DIR}/${name}"
+            DOWNLOAD_EXTRACT_TIMESTAMP true
+        )
+    endif()
+
+    FetchContent_MakeAvailable(${name})
+
+    message(TRACE "Macro: fetch_github_dependency(${name}) - done")
+endmacro()
+
 message(STATUS "Fetching dependencies ...")
 
 include(FetchContent)
 
-message(STATUS "Fetching dependency choc")
-
-FetchContent_Declare(
+fetch_github_dependency(
     choc
     GIT_REPOSITORY https://github.com/Tracktion/choc.git
     GIT_TAG 85149958b6d0e51885eefba8816b51798570b54b
+    URL_MD5 ffdb942dcaa64a716239d37c620e5eb2
     SOURCE_DIR "${FETCHCONTENT_BASE_DIR}/choc-src/include/choc"
+    # USE_GIT
 )
 
-FetchContent_MakeAvailable(choc)
+fetch_github_dependency(
+    readerwriterqueue
+    GIT_REPOSITORY https://github.com/cameron314/readerwriterqueue.git
+    GIT_TAG v1.0.6
+    URL_MD5 a22feb68840ba44cbc2b01853fb4a503
+    # USE_GIT
+)
 
-message(STATUS "Fetching dependency choc - done")
-
-message(STATUS "Fetching dependency iPlug2")
-
-FetchContent_Declare(
-    iPlug2
+fetch_github_dependency(
+    iplug2
     GIT_REPOSITORY https://github.com/rorywalsh/iPlug2.git
-    GIT_TAG 70234c3f5923586db44e91fa8b4a66a9cb22230e
+    GIT_TAG baa9a03bf2488a809e9020cd4c1dc69b20b52913
+    URL_MD5 fe30ea1822d9935ce9dbd1ef0adb86c2
+    # USE_GIT
 )
 
-FetchContent_MakeAvailable(iPlug2)
-
-message(STATUS "Fetching dependency iPlug2 - done")
-
-message(STATUS "Fetching dependency iPlug2_dependencies")
+message(STATUS "Fetching dependency iplug2_dependencies")
 
 if(CMAKE_SYSTEM_NAME STREQUAL "Darwin")
-    set(iPlug2_dependencies_zip_file "IPLUG2_DEPS_MAC")
-    set(iPlug2_dependencies_url_md5 b85fadfaba0806ce57b4c4d486453451)
+    set(iplug2_dependencies_zip_file "IPLUG2_DEPS_MAC")
+    set(iplug2_dependencies_url_md5 b85fadfaba0806ce57b4c4d486453451)
 elseif(CMAKE_SYSTEM_NAME STREQUAL "Windows")
-    set(iPlug2_dependencies_zip_file "IPLUG2_DEPS_WIN")
+    set(iplug2_dependencies_zip_file "IPLUG2_DEPS_WIN")
     # TODO: Update iPlug2 dependencies zip file md5 hash for Windows.
-    set(iPlug2_dependencies_url_md5 ef6cec00694e5791e21368483f8f8a65)
+    set(iplug2_dependencies_url_md5 ef6cec00694e5791e21368483f8f8a65)
 else()
     message(FATAL_ERROR "Unsupported system for iPlug2: ${CMAKE_SYSTEM_NAME}")
 endif()
 
-# Fetch iPlug2 dependencies into the iPlug2 source directory's Build subdirectory, creating it if needed.
+# Fetch iPlug2 dependencies into the iPlug2 source directory's Dependencies/Build subdirectory, creating it if needed.
 # This replicates the behavior of the iPlug2 download-prebuilt-libs.sh script.
 FetchContent_Declare(
-    iPlug2_dependencies
-    URL https://github.com/iPlug2/iPlug2/releases/download/setup/${iPlug2_dependencies_zip_file}.zip
-    URL_MD5 ${iPlug2_dependencies_url_md5}
-    DOWNLOAD_DIR "${FETCHCONTENT_BASE_DIR}/iPlug2_dependencies"
+    iplug2_dependencies
+    URL https://github.com/iPlug2/iPlug2/releases/download/setup/${iplug2_dependencies_zip_file}.zip
+    URL_MD5 ${iplug2_dependencies_url_md5}
+    DOWNLOAD_DIR "${FETCHCONTENT_BASE_DIR}/iplug2_dependencies"
     DOWNLOAD_EXTRACT_TIMESTAMP true
     SOURCE_DIR "${iplug2_SOURCE_DIR}/Dependencies/Build"
 )
 
-FetchContent_MakeAvailable(iPlug2_dependencies)
+FetchContent_MakeAvailable(iplug2_dependencies)
 
-message(STATUS "Fetching dependency iPlug2_dependencies - done")
+message(STATUS "Fetching dependency iplug2_dependencies - done")
 
-message(STATUS "Fetching dependency iPlug2_vst3_sdk")
 
-# Set these variables to disable building VST3 plugin examples, VST3 hosting examples, and VSTGUI support.
-#
-# NB: The original iPlug2 script removes the associated directories for these features, but that causes errors when
-# using the CMake FetchContent API.
-set(SMTG_ENABLE_VST3_PLUGIN_EXAMPLES OFF)
-set(SMTG_ENABLE_VST3_HOSTING_EXAMPLES OFF)
-set(SMTG_ENABLE_VSTGUI_SUPPORT OFF)
+if(vst3 STREQUAL "${CABBAGE_BUILD_PLUGIN_TYPE}")
+    message(STATUS "Fetching dependency vst3sdk")
 
-FetchContent_Declare(
-    iPlug2_vst3_sdk
-    GIT_REPOSITORY https://github.com/steinbergmedia/vst3sdk.git
-    GIT_TAG cc2adc90382dded9e347caf74e4532f1458715db
-    GIT_SUBMODULES "base" "cmake" "pluginterfaces" "public.sdk" "vstgui4"
-    SOURCE_DIR "${iplug2_SOURCE_DIR}/Dependencies/iPlug/VST3_SDK"
-)
+    # Set these variables to disable building the VST3 SDK plugin examples, hosting examples, and gui support.
+    # NB: The original iPlug2 script deletes the associated directories for these features after cloning, but this
+    # causes errors when using the CMake FetchContent API, so we do the same thing by setting these variables to OFF.
+    set(SMTG_ENABLE_VST3_PLUGIN_EXAMPLES OFF)
+    set(SMTG_ENABLE_VST3_HOSTING_EXAMPLES OFF)
+    set(SMTG_ENABLE_VSTGUI_SUPPORT OFF)
 
-FetchContent_MakeAvailable(iPlug2_vst3_sdk)
+    # Fetch the VST3 SDK from the Git repository instead of the Github .tar.gz because we need its submodules and the
+    # .tar.gz doesn't have them.
+    FetchContent_Declare(
+        vst3sdk
+        GIT_REPOSITORY https://github.com/steinbergmedia/vst3sdk.git
+        GIT_TAG cc2adc90382dded9e347caf74e4532f1458715db
+        GIT_SUBMODULES "base" "cmake" "pluginterfaces" "public.sdk" "vstgui4"
+        SOURCE_DIR "${iplug2_SOURCE_DIR}/Dependencies/iPlug/VST3_SDK"
+    )
 
-message(STATUS "Fetching dependency iPlug2_vst3_sdk - done")
+    FetchContent_MakeAvailable(vst3sdk)
 
-message(STATUS "Fetching dependency iPlug2_clap_sdk")
-
-FetchContent_Declare(
-    iPlug2_clap_sdk
-    GIT_REPOSITORY https://github.com/free-audio/clap.git
-    GIT_TAG 27f20f81dec40b930d79ef429fd35dcc2d45db5b
-    DOWNLOAD_DIR "${FETCHCONTENT_BASE_DIR}/iPlug2_clap_sdk"
-    SOURCE_DIR "${iplug2_SOURCE_DIR}/Dependencies/iPlug/CLAP_SDK"
-)
-
-FetchContent_MakeAvailable(iPlug2_clap_sdk)
-
-message(STATUS "Fetching dependency iPlug2_clap_sdk - done")
-
-message(STATUS "Fetching dependency iPlug2_clap_helpers")
-
-FetchContent_Declare(
-    iPlug2_clap_helpers
-    GIT_REPOSITORY https://github.com/free-audio/clap-helpers.git
-    GIT_TAG 59791394dc26637d9425c2745233a979602be2a7
-    DOWNLOAD_DIR "${FETCHCONTENT_BASE_DIR}/iPlug2_clap_helpers"
-    SOURCE_DIR "${iplug2_SOURCE_DIR}/Dependencies/iPlug/CLAP_HELPERS"
-)
-
-FetchContent_MakeAvailable(iPlug2_clap_helpers)
-
-message(STATUS "Fetching dependency iPlug2_clap_helpers - done")
-
-message(STATUS "Fetching dependency iPlug2_wam_sdk")
-
-FetchContent_Declare(
-    iPlug2_wam_sdk
-    GIT_REPOSITORY https://github.com/iplug2/api.git
-    GIT_TAG e0fb276b5a16df4d3b7419b14d6347593ebd8846
-    DOWNLOAD_DIR "${FETCHCONTENT_BASE_DIR}/iPlug2_wam_sdk"
-    SOURCE_DIR "${iplug2_SOURCE_DIR}/Dependencies/iPlug/WAM_SDK"
-)
-
-FetchContent_MakeAvailable(iPlug2_wam_sdk)
-
-message(STATUS "Fetching dependency iPlug2_wam_sdk - done")
-
-message(STATUS "Fetching dependency iPlug2_wam_audioworklet_polyfill")
-
-FetchContent_Declare(
-    iPlug2_wam_audioworklet_polyfill
-    GIT_REPOSITORY https://github.com/iplug2/audioworklet-polyfill
-    GIT_TAG 771ddc51076c7d0c5db6ac773f304a2a85a4a8ae
-    SOURCE_DIR "${iplug2_SOURCE_DIR}/Dependencies/iPlug/WAM_AWP"
-)
-
-FetchContent_MakeAvailable(iPlug2_wam_audioworklet_polyfill)
-
-message(STATUS "Fetching dependency iPlug2_wam_audioworklet_polyfill - done")
-
-message(STATUS "Fetching dependency readerwriterqueue")
-
-FetchContent_Declare(
-    readerwriterqueue
-    GIT_REPOSITORY https://github.com/cameron314/readerwriterqueue.git
-    GIT_TAG v1.0.6
-)
-
-FetchContent_MakeAvailable(readerwriterqueue)
-
-message(STATUS "Fetching dependency readerwriterqueue - done")
+    message(STATUS "Fetching dependency vst3sdk - done")
+endif()
 
 message(STATUS "Fetching dependencies - done")
