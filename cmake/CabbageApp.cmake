43f67bef2d9eb65ff6c9a478a8e24441f0d2c6db
Rory Walsh <rorywalsh@ear.ie>
2024-12-04 15:04:31 +0000

    updating windows CI builds

--- cmake/CabbageApp.cmake
+++ cmake/CabbageApp.cmake
@@ -14,8 +14,7 @@ add_executable(${CABBAGE_PROJECT_NAME} MACOSX_BUNDLE
 
 iplug_target_add(${CABBAGE_PROJECT_NAME} PUBLIC
     DEFINE
-        CabbageApp
-# 
+        CabbageApp 
     INCLUDE
         "${CMAKE_SOURCE_DIR}/resources"
     LINK
@@ -33,11 +32,19 @@ if(WIN32)
     # Having to do this here because I can't seem to get it to work during the generation process. 
     # This is an ugly hack, but I'm out of ideas, none of the recommended CMake ways of changing the 
     # runtime library work.
-    add_custom_command(
-        TARGET ${CABBAGE_PROJECT_NAME} PRE_BUILD
-        COMMAND ${CMAKE_COMMAND} -E echo "Modifying ${CABBAGE_PROJECT_NAME}.vcxproj file"
-        COMMAND powershell -Command "(Get-Content ${CMAKE_BINARY_DIR}/${CABBAGE_PROJECT_NAME}.vcxproj) -replace '<RuntimeLibrary>MultiThreadedDebug</RuntimeLibrary>', '<RuntimeLibrary>MultiThreadedDebugDLL</RuntimeLibrary>' | Set-Content ${CMAKE_BINARY_DIR}/${CABBAGE_PROJECT_NAME}.vcxproj"
-    )
+    if(CMAKE_BUILD_TYPE STREQUAL "Debug")
+        add_custom_command(
+            TARGET ${CABBAGE_PROJECT_NAME} PRE_BUILD
+            COMMAND ${CMAKE_COMMAND} -E echo "Modifying ${CABBAGE_PROJECT_NAME}.vcxproj file"
+            COMMAND powershell -Command "(Get-Content ${CMAKE_BINARY_DIR}/${CABBAGE_PROJECT_NAME}.vcxproj) -replace '<RuntimeLibrary>MultiThreadedDebug</RuntimeLibrary>', '<RuntimeLibrary>MultiThreadedDebugDLL</RuntimeLibrary>' | Set-Content ${CMAKE_BINARY_DIR}/${CABBAGE_PROJECT_NAME}.vcxproj"
+        )
+    else()
+        add_custom_command(
+            TARGET ${CABBAGE_PROJECT_NAME} PRE_BUILD
+            COMMAND ${CMAKE_COMMAND} -E echo "Modifying ${CABBAGE_PROJECT_NAME}.vcxproj file"
+            COMMAND powershell -Command "(Get-Content ${CMAKE_BINARY_DIR}/${CABBAGE_PROJECT_NAME}.vcxproj) -replace '<RuntimeLibrary>MultiThreaded</RuntimeLibrary>', '<RuntimeLibrary>MultiThreadedDLL</RuntimeLibrary>' | Set-Content ${CMAKE_BINARY_DIR}/${CABBAGE_PROJECT_NAME}.vcxproj"
+        )
+    endif()
     target_link_options(${CABBAGE_PROJECT_NAME} PRIVATE "/SUBSYSTEM:WINDOWS")
 else()
     target_link_options(${CABBAGE_PROJECT_NAME} PRIVATE LINKER:-adhoc_codesign)
