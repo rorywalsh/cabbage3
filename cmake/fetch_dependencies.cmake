19db8e5b423f3f2f8021eba39e4bb54ddd0a88a3
rorywalsh <rorywalsh@ear.ie>
2025-01-29 15:33:57 +0000

    updating cmake

--- cmake/fetch_dependencies.cmake
+++ cmake/fetch_dependencies.cmake
@@ -30,8 +30,8 @@ if(LINUX)
     fetch_github_dependency(
         iplug2
         GIT_REPOSITORY https://github.com/rorywalsh/iPlug2.git
-        GIT_TAG 28ffd0dbef39a18584cdd1682485151f362be7c4
-        URL_MD5 cf28edb5b8bfb14d7a7beae9da77fbb7
+        GIT_TAG 5c09f68214a5f4471c91ceb146e7d07dca65b077
+        URL_MD5 d0e4af54c75b031083d7f8db807a96ae
         # USE_GIT
     )
 else()
