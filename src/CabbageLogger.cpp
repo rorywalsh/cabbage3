650627f223dae3637579c1cc37338424b2478bc7
rorywalsh <rorywalsh@ear.ie>
2025-08-17 14:02:10 +0200

    updating plugin info header and update lattice deps

--- src/CabbageLogger.cpp
+++
@@ -1,9 +0,0 @@
-#include "CabbageLogger.h"
-
-namespace cabbage {
-
-// Static member definitions
-FileLogger* FileLogger::instance = nullptr;
-std::mutex FileLogger::instanceMutex;
-
-} // namespace cabbage
--- src/CabbagePluginInfo.h
+++ src/CabbagePluginInfo.h
@@ -85,12 +85,12 @@ namespace {
 inline const clap_plugin_descriptor* getDescriptor() {
     static const clap_plugin_descriptor descriptor = {
         .clap_version = CLAP_VERSION,
-    #if VST3_SDK
-        .id = pluginInfo.id.c_str(),
-        .name = pluginInfo.name.c_str(),
-    #else
+    #if CABBAGE_AU_BUILD == 1
         .id = "com.cabbageaudio.1d47",
         .name = "Cabbage1d47Plugin",
+    #else
+        .id = pluginInfo.id.c_str(),
+        .name = pluginInfo.name.c_str(),       
     #endif
         .vendor = "CabbageAudio",
         .url = "https://cabbageaudio.com",
