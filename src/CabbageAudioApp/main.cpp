92de15a9f0b61e3918fb89a5227708832e07dac6
rorywalsh <rorywalsh@ear.ie>
2026-01-16 15:36:46 +0000

    Updating license

--- src/CabbageAudioApp/main.cpp
+++ src/CabbageAudioApp/main.cpp
@@ -1,3 +1,22 @@
+/*
+ * Copyright (c) 2024 Rory Walsh
+ *
+ * This file is part of Cabbage3
+ *
+ * Cabbage3 is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Cabbage3 is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Cabbage3.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 #include "CabbageAudioApp.h"
 #include <thread>
 #include <chrono>
--- src/CabbageColours.h
+++ src/CabbageColours.h
@@ -1,9 +1,20 @@
 /*
  * Copyright (c) 2024 Rory Walsh
  *
- * Cabbage3 is licensed under the MIT License. See the LICENSE file for details.
- * This software is provided "as-is", without any express or implied warranty.
- * See the LICENSE file for more details.
+ * This file is part of Cabbage3
+ *
+ * Cabbage3 is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Cabbage3 is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Cabbage3.  If not, see <https://www.gnu.org/licenses/>.
  */
 
 #ifndef CabbageColours_h
